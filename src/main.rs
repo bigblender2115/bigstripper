7cb0b13936c117190f1725f42649262bce6069be
bigblender2115 <shauryapaka@gmail.com>
2026-05-24 13:21:01 +0530

    arg parsing

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,31 @@ use img_parts::jpeg::Jpeg;
 use img_parts::png::Png;
 use std::fs;
 use std::path::PathBuf;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+    input: PathBuf,
+    #[arg(short, long)]
+    output: Option<PathBuf>,
+    #[clap(long, short, action)]
+    dry_run: bool,
+}
 
 fn main() {
-    let input = PathBuf::from("test.jpg");
-    let output = PathBuf::from("test_o.jpg");
+    let args = Cli::parse();
+    let input = args.input;
+    let output = match args.output {
+        Some(path) => path,
+        None => {
+            let stem = input.file_stem().unwrap().to_str().unwrap();
+            let ext = input.extension().unwrap().to_str().unwrap();
+            PathBuf::from(format!("{stem}_stripped.{ext}"))
+        },
+    };
+
+    let mut has_exif_xmp = false;
+    let mut has_iptc = false;
 
     let bytes = fs::read(&input).expect("couldnt read file");
     let extension = input
@@ -20,8 +41,14 @@ fn main() {
             jpeg.segments_mut().retain(|seg| {
                 let marker = seg.marker();
                 match marker {
-                    0xED => false,
-                    0xE1 => false,
+                    0xED => {
+                        has_exif_xmp = true;
+                        false
+                    },
+                    0xE1 => {
+                        has_iptc = true;
+                        false
+                    },
                     _ => true,
                 }
             });
@@ -38,8 +65,27 @@ fn main() {
         }
         _ => panic!("unsupported file format"),
     };
-    
-    fs::write(&output, out_bytes).expect("cant write to this file");
 
-    println!("done -> {:?}", output);
+    if args.dry_run {
+        if has_exif_xmp {
+            println!("exif/xmp chunk found");
+        } else {
+            println!("exif/xmp chunk not found");
+        }
+        if has_iptc {
+            println!("iptc chunk found");
+        } else {
+            println!("iptc chunk not found")
+        }
+        if !has_exif_xmp && !has_iptc {
+            println!("No metadata to strip");
+        }
+    } else {
+        if !has_exif_xmp && !has_iptc {
+            println!("No metadata to strip");
+        } else {
+            fs::write(&output, out_bytes).expect("cant write to this file");
+            println!("done -> {:?}", output);
+        }
+    }
 }
